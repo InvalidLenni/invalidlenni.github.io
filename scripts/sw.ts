8aa5c1d57b06f77aa087459ecc019bda552e40e7
InvalidLenni <68335355+InvalidLenni@users.noreply.github.com>
2022-01-12 20:02:55 +0100

    Update and rename sw.js to sw.ts

---
+++ scripts/sw.ts
@@ -0,0 +1,3 @@
+importScripts('https://arc.io/arc-sw-core.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+workbox.routing.registerRoute(/.*/, new workbox.strategies.NetworkFirst());
