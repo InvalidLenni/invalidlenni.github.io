5ce9d7212127299e5fd328a34e806ea11040ca9e
InvalidLenni <68335355+InvalidLenni@users.noreply.github.com>
2022-01-18 14:14:47 +0100

    Moved to root directory

--- scripts/sw.js
+++
@@ -1,7 +0,0 @@
-importScripts('https://arc.io/arc-sw-core.js')
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
-
-workbox.routing.registerRoute(
-  /.*/,
-  new workbox.strategies.NetworkFirst()
-);
