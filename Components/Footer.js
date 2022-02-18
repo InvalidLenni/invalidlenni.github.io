4b70aeebb41c66cd0b0b5818ec9ca7d9ef1094f3
InvalidLenni <68335355+InvalidLenni@users.noreply.github.com>
2022-02-18 16:16:48 +0100

    Add files via upload

---
+++ Components/Footer.js
@@ -0,0 +1,19 @@
+const element = 
+  <footer class="mastfoot mb-auto">
+    <div class="container-fluid mb-auto">
+        <div class="">
+          <div class="">    
+            <p style="position:static; text-align: center; padding-bottom: 10%; font-size: 19px; margin: 0px; padding: 0px;"><a href="https://invalidlenni.de/"
+                      style="text-decoration: none; color: white;">Made with 💖 by</a>InvalidLenni ©
+                  2021 - <script>document.write((new Date()).getFullYear()) </script>. All<br>
+                  rights reserved. Source code available at <a href="https://github.com/InvalidLenni/invalidlenni.github.io/">Github</a> and the website will be hosted by <a href="https://pages.github.com/">Github (Pages)</a>. Badges hosted by <a href="https://shields.io/">Shields</a> and <a href="https://github.com/">Github</a>. You can support me <a href="https://deinserverhost.de/store/aff.php?aff=4224">here</a> with money. You can you join my discord server or follow my twitter account <strong>without money</strong>.</br></p>
+          </div>
+      </div>
+    </div>
+  </footer>  
+
+ReactDOM.render(
+    element,
+    document.getElementById('react-footer')
+  );
+
